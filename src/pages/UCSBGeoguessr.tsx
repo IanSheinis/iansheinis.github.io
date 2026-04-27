d5b11ec234d9b59ace0aa376436d3810aaa8afd5
Ian Sheinis <iansheinis@gmail.com>
2026-04-26 19:26:53 -0700

    UCSBGeoguessr diagram draft

--- src/pages/UCSBGeoguessr.tsx
+++ src/pages/UCSBGeoguessr.tsx
@@ -1,16 +1,29 @@
 import gameplay from '../assets/Ucsbgeoguessr_gameplay.mp4'
+import UcsbgeoguessrDiagram from '../assets/ucsbgeoguessr_diagram.png'
+import ProjectSection from '../components/ProjectSection'
 import VideoSection from '../components/VideoSection'
  export default function UCSBGeoGuessr() {
     return (
         <>
 
         <VideoSection
-                videoSrc={gameplay}
-                title="UCSBGeoGuessr"
-                labels={[
-                    "We wanted to build a GeoGuessr clone as a fun way for UCSB students to familiarize themselves with the campus and test their knowledge.",
-                    "Problem is, how do we store hundreds of images of campus without adding hundreds of MB to our app???" 
+            videoSrc={gameplay}
+            title="UCSBGeoGuessr"
+            labels={[
+                "We wanted to build a GeoGuessr clone as a fun way for UCSB students to familiarize themselves with the campus and test their knowledge.",
+                "Problem is, how do we store hundreds of images of campus without adding hundreds of MB to our app???" 
+            ]}
+        />
+
+        <ProjectSection
+                title="LLM pipeline"
+                description="I built a pipeline to match patients to clinical trials using an LLM"
+                bullets={[
+                    { label: "Asynchronous", text: "Bedrock evaluation time exceeds API Gateway's 29-second timeout, therefore requiring asynchronous processing" },
+                    { label: "Workflow", text: "Frontend POSTs patient info and trial ID to queue the evaluation, then polls for results via GET"}
                 ]}
+                imageSrc={UcsbgeoguessrDiagram}
+                imageAlt="Centrova architecture diagram"
         />
 
         </>
